a3b58b617017e053dd58d224e7a01adf66f33667
Mitch Conquer <mitch.conquer@gonimbly.com>
2017-05-28 15:18:02 -0700

    Add own reducers

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,11 +1,16 @@
 // @flow
-import { combineReducers } from 'redux';
-import { routerReducer as router } from 'react-router-redux';
-import counter from './counter';
+import { combineReducers } from 'redux'
+import { routerReducer as router } from 'react-router-redux'
+
+import files from './files'
+import filter from './filter'
+import subtitles from './subtitles'
 
 const rootReducer = combineReducers({
-  counter,
-  router,
-});
+  files,
+  filter,
+  subtitles,
+  router
+})
 
-export default rootReducer;
+export default rootReducer
