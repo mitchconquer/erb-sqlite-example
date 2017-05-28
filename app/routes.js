d54782b3f3b7b8e8d93b15469a07cef3cac49555
Mitch Conquer <mitch.conquer@gonimbly.com>
2017-05-28 16:51:26 -0700

    Update routes to use own containers

--- app/routes.js
+++ app/routes.js
@@ -3,14 +3,14 @@ import React from 'react';
 import { HashRouter as Router } from 'react-router-dom';
 import { Switch, Route } from 'react-router';
 import App from './containers/App';
-import HomePage from './containers/HomePage';
-import CounterPage from './containers/CounterPage';
+import HomePage from './containers/HomePage'
+import EditPage from './containers/EditPage'
 
 export default () => (
   <Router>
     <App>
       <Switch>
-        <Route path="/counter" component={CounterPage} />
+        <Route path="/edit" component={EditPage} />
         <Route path="/" component={HomePage} />
       </Switch>
     </App>
